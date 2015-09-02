76bdb67c7f1ec877ca477a9bf698ee7ca22f1687
Jose Silva <jlsilva073@misena.edu.co>
2015-09-02 16:07:07 -0500

    termine de hacer prueba

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+<?php echo "hola linda";?>
+
+</body>
+</html>
---
+++ indexdos.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> bhsdcgyidg </title>
+</head>
+<body>
+ <?php "este es de prueba"; ?>
+</body>
+</html>
